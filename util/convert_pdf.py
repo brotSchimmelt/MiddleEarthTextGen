a9a506678ab123bb35192f9838428eb9106feb93
brotSchimmelt <tknebler+git@googlemail.com>
2023-02-10 19:49:55 +0100

    format files with black

--- util/convert_pdf.py
+++ util/convert_pdf.py
@@ -1,30 +1,27 @@
 import pdfplumber
 from helper import remove_spaces
 
-
 INPUT_PDF_PATH = "../text_files/x.pdf"
 OUTPUT_TXT_PATH = "../text_files/txt/x.txt"
 
 
 with pdfplumber.open(INPUT_PDF_PATH) as pdf:
-    
     total_num_pages = len(pdf.pages)
-    print(f'total number of pages: {total_num_pages}')
-    
-    text = ''
+    print(f"total number of pages: {total_num_pages}")
+
+    text = ""
     for i in range(total_num_pages):
         current_page = pdf.pages[i]
-        
+
         # # only for the third volume
         # # skip first line on each page
         # raw = current_page.extract_text()
-        # raw_without_first_line = '\n'.join(raw.split('\n')[1:]) 
+        # raw_without_first_line = '\n'.join(raw.split('\n')[1:])
         # text += remove_spaces(raw_without_first_line) + '\n'
-        
-        text += remove_spaces(current_page.extract_text()) + '\n'
+
+        text += remove_spaces(current_page.extract_text()) + "\n"
 
 # write to file
-with open(OUTPUT_TXT_PATH, 'w') as f:
+with open(OUTPUT_TXT_PATH, "w") as f:
     f.write(text)
-print('done.')
-    
+print("done.")
--- util/helper.py
+++ util/helper.py
@@ -1,11 +1,11 @@
 import re
 
 
-def remove_spaces(s):    
+def remove_spaces(s):
     # replaces multiple whitespaces with a single space
-    return re.sub(' +', ' ', s.replace('\t', ' ')).replace(' \n ', '\n')
+    return re.sub(" +", " ", s.replace("\t", " ")).replace(" \n ", "\n")
 
 
 def remove_spaces_concisely(s):
     # convert every pdf page to a single line in the output
-    return re.sub('\s+', ' ', s).replace(' \n', '\n')
+    return re.sub("\s+", " ", s).replace(" \n", "\n")
