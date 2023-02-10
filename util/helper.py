9c546488cf63b9f588ba4f0f0dd41afe91031623
brotSchimmelt <tknebler+git@googlemail.com>
2023-02-10 23:11:07 +0100

    add typing

--- util/helper.py
+++ util/helper.py
@@ -1,11 +1,11 @@
 import re
 
 
-def remove_spaces(s):
-    # replaces multiple whitespaces with a single space
+def remove_spaces(s: str) -> str:
+    """Replaces multiple whitespaces with a single space."""
     return re.sub(" +", " ", s.replace("\t", " ")).replace(" \n ", "\n")
 
 
-def remove_spaces_concisely(s):
-    # convert every pdf page to a single line in the output
+def remove_spaces_concisely(s: str) -> str:
+    """Convert every pdf page to a single line in the output."""
     return re.sub("\s+", " ", s).replace(" \n", "\n")
